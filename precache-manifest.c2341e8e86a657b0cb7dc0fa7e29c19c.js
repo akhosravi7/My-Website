09ca3872a60c666767afaf8c632056d43c908072
akhosravi7 <alikhosravi1000@gmail.com>
2020-01-01 12:01:43 -0500

    Updates

---
+++ precache-manifest.c2341e8e86a657b0cb7dc0fa7e29c19c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ab9116cbe7a3da0339aa63976116744",
+    "url": "/My-Website/index.html"
+  },
+  {
+    "revision": "7b43f3152a73478b9722",
+    "url": "/My-Website/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "07158fe9fbc8f8d950f8",
+    "url": "/My-Website/static/css/main.fa7b9723.chunk.css"
+  },
+  {
+    "revision": "7b43f3152a73478b9722",
+    "url": "/My-Website/static/js/2.40b233db.chunk.js"
+  },
+  {
+    "revision": "07158fe9fbc8f8d950f8",
+    "url": "/My-Website/static/js/main.07287715.chunk.js"
+  },
+  {
+    "revision": "4e016758bc708445f074",
+    "url": "/My-Website/static/js/runtime-main.5ff06795.js"
+  },
+  {
+    "revision": "647ad2c91315381544db14d0da61f215",
+    "url": "/My-Website/static/media/AKRESUME.647ad2c9.jpg"
+  },
+  {
+    "revision": "3f9b7cb0a9c3736ce74d9385bd1486bd",
+    "url": "/My-Website/static/media/Beach.3f9b7cb0.jpg"
+  },
+  {
+    "revision": "e827ae3a597ece5ef77079442b1c89c5",
+    "url": "/My-Website/static/media/Suit.e827ae3a.JPG"
+  }
+]);
